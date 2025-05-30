78be3988caa1232b9882454df19ea1906c43b9a8
Marcu702 <marcus184184@gmail.com>
2025-05-30 09:17:15 -0400

    Math Training Game Code
    This will be the slow progress of the creation for a Math Training Game

--- CPTMarcus.java
+++ CPTMarcus.java
@@ -2,9 +2,32 @@ import arc.*;
 
 public class CPTMarcus{
 	public static void main(String[] args){
-		Console con = new Console();
+		Console con = new Console("Math Training Game",1920,1550);
 		
-		con.println("Hello World");
+//Variables	
+		String strPlayerName;
+//Data
+//ONE MASTER FILE
+//SUB FILES - (quiz)
+//Game interface
+
+//PLAY GAME
+//LEADBOARD
+//ADD QUIZ
+//QUIT BUTTON
+
+//Gameplay
+
+//ASK FOR NAME
+//SHOW THE QUIZ THAT CAN BE CHOSEN
+//SHOW THE QUESTION THEN LET TEM ANSWER WITH TEXT
+//TAKE THE PLAYERS ANSWER TO SEE IF IT MATCHES
+//KEEP TRACK OF THE CORRECT ANSWERS, QUIZ NAME, AMOUNT ANSWERED, NAME, AND SCORE.
+
+
+
+
+		
 		}
 	}
 		
