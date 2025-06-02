c278cda80ff80d131cf849b26bc402dea9d3f6ad
Marcu702 <marcus184184@gmail.com>
2025-06-02 09:37:42 -0400

    Added logo + font
    Made many changes towards the main menu screen, almost completing it.

--- CPTMarcus.java
+++ CPTMarcus.java
@@ -1,17 +1,28 @@
 import arc.*;
 import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.awt.Font;
 
 public class CPTMarcus{
 	public static void main(String[] args){
 		Console con = new Console("Math Training Game",960,540);
 //Main Menu Screen
+		Font fnt = con.loadFont("font1.ttf", 30);
+		BufferedImage Logo = con.loadImage("Logo1.jpeg");
 		con.setDrawColor(Color.WHITE);
 		con.fillRect(0,0,960,540);
+		con.setDrawFont(fnt);
 		con.setDrawColor(Color.BLACK);
 		con.drawString("Math Training Game", 380, 215);
 		con.drawString("Play [ P ] ", 380, 250);
 		con.drawString("Leaderboard [ L ]", 380, 285);
-		con.drawString("Add Quiz [ + ]", 380, 320);
+		con.drawString("Add Quiz [ A ]", 380, 320);
+		con.drawString("Quit [ Q ]", 380, 355);
+		con.drawImage(Logo, 385,1);
+
+		
+		con.repaint();
+	
 //Variables	
 		String strPlayerName;
 //Data
