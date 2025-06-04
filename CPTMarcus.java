1b6e3a413f5fff8f206890d2ed873a58f12193e6
Marcu702 <marcus184184@gmail.com>
2025-06-04 08:31:53 -0400

    Button input
    Added a RETURN function after pressing the PLAY button

--- CPTMarcus.java
+++ CPTMarcus.java
@@ -6,11 +6,22 @@ import java.awt.Font;
 public class CPTMarcus{
 	public static void main(String[] args){
 		Console con = new Console("Math Training Game",960,540);
+//Varibles 
+
+		String strPlayerName;
+		char chrMenuOption;
+		
 //Main Menu Screen
-		Font fnt = con.loadFont("font1.ttf", 30);
+
+//Font and Logo
+		Font fnt = con.loadFont("", 30);
 		BufferedImage Logo = con.loadImage("Logo1.jpeg");
+		
+//Background
 		con.setDrawColor(Color.WHITE);
 		con.fillRect(0,0,960,540);
+		
+//Text
 		con.setDrawFont(fnt);
 		con.setDrawColor(Color.BLACK);
 		con.drawString("Math Training Game", 380, 215);
@@ -24,7 +35,45 @@ public class CPTMarcus{
 		con.repaint();
 	
 //Variables	
-		String strPlayerName;
+
+
+		chrMenuOption = con.getChar();
+		System.out.println("entered Letter: "+chrMenuOption);
+		if(chrMenuOption == 'P' || chrMenuOption == 'p'){
+			con.setBackgroundColor(Color.WHITE);
+			chrMenuOption = con.getChar();
+			if(chrMenuOption == 'R' || chrMenuOption == 'r'){
+			con.setBackgroundColor(Color.WHITE);
+			con.setDrawColor(Color.BLACK);
+			con.drawString("Math Training Game", 380, 215);
+			con.drawString("Play [ P ] ", 380, 250);
+			con.drawString("Leaderboard [ L ]", 380, 285);
+			con.drawString("Add Quiz [ A ]", 380, 320);
+			con.drawString("Quit [ Q ]", 380, 355);
+			con.drawImage(Logo, 385,1);
+			con.repaint();
+		}
+		else if(chrMenuOption == 'L' || chrMenuOption == 'l' ){
+			con.setBackgroundColor(Color.WHITE);
+		}
+		else if(chrMenuOption == 'A' || chrMenuOption == 'a'){
+			con.setBackgroundColor(Color.WHITE);
+		}
+		else if(chrMenuOption == 'Q' || chrMenuOption == 'q'){
+			con.setBackgroundColor(Color.WHITE);
+		}
+		else if(chrMenuOption == 'R' || chrMenuOption == 'r'){
+			con.setBackgroundColor(Color.WHITE);
+			con.setDrawColor(Color.BLACK);
+			con.drawString("Math Training Game", 380, 215);
+			con.drawString("Play [ P ] ", 380, 250);
+			con.drawString("Leaderboard [ L ]", 380, 285);
+			con.drawString("Add Quiz [ A ]", 380, 320);
+			con.drawString("Quit [ Q ]", 380, 355);
+			con.drawImage(Logo, 385,1);
+			con.repaint();
+		}
+	
 //Data
 //ONE MASTER FILE
 //SUB FILES - (quiz)
@@ -49,5 +98,6 @@ public class CPTMarcus{
 		
 		}
 	}
+}
 		
 
