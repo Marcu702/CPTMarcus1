b832b59b9a24f75ba90993e69625e64945afda04
Marcu702 <marcus184184@gmail.com>
2025-06-04 09:27:30 -0400

    Add Starting Array
    Today, I added the quiz array. where if you press P again after pressing play the quizzes will show up

--- CPTMarcus.java
+++ CPTMarcus.java
@@ -8,8 +8,9 @@ public class CPTMarcus{
 		Console con = new Console("Math Training Game",960,540);
 //Varibles 
 
-		String strPlayerName;
+		String strQuiz[];
 		char chrMenuOption;
+		int intCount;
 		
 //Main Menu Screen
 
@@ -34,13 +35,17 @@ public class CPTMarcus{
 		
 		con.repaint();
 	
-//Variables	
-
-
+//Button Functions	
 		chrMenuOption = con.getChar();
 		System.out.println("entered Letter: "+chrMenuOption);
+//Play Button Funciton
+
+	
 		if(chrMenuOption == 'P' || chrMenuOption == 'p'){
 			con.setBackgroundColor(Color.WHITE);
+			chrMenuOption = con.getChar();
+//Return Menu Function
+			System.out.println("entered Letter: "+chrMenuOption);
 			chrMenuOption = con.getChar();
 			if(chrMenuOption == 'R' || chrMenuOption == 'r'){
 			con.setBackgroundColor(Color.WHITE);
@@ -52,27 +57,36 @@ public class CPTMarcus{
 			con.drawString("Quit [ Q ]", 380, 355);
 			con.drawImage(Logo, 385,1);
 			con.repaint();
-		}
-		else if(chrMenuOption == 'L' || chrMenuOption == 'l' ){
+			}else if(chrMenuOption == 'p' || chrMenuOption == 'P'){
+				con.setBackgroundColor(Color.BLACK);
+				con.println("What QUIZ would you like to do?");
+				con.println("(SELECT THE NUMBER CORRESPONDING WITH THE QUIZ ORDER)");
+				strQuiz = new String[100];
+				TextInputFile quizoptions = new TextInputFile("Quizzies.txt");
+					for(intCount = 0; intCount < 5; intCount++){
+						strQuiz[intCount] = quizoptions.readLine();
+							}
+						quizoptions.close();
+						for(intCount = 0; intCount < 5; intCount++){
+							con.println(strQuiz[intCount]);
+				}
+					
+				
+				
+			}
+		
+		}else if(chrMenuOption == 'L' || chrMenuOption == 'l' ){
+			con.setBackgroundColor(Color.WHITE);
+
+		
+		}else if(chrMenuOption == 'A' || chrMenuOption == 'a'){
+			con.setBackgroundColor(Color.WHITE);			
+
+		
+		}else if(chrMenuOption == 'Q' || chrMenuOption == 'q'){
 			con.setBackgroundColor(Color.WHITE);
 		}
-		else if(chrMenuOption == 'A' || chrMenuOption == 'a'){
-			con.setBackgroundColor(Color.WHITE);
-		}
-		else if(chrMenuOption == 'Q' || chrMenuOption == 'q'){
-			con.setBackgroundColor(Color.WHITE);
-		}
-		else if(chrMenuOption == 'R' || chrMenuOption == 'r'){
-			con.setBackgroundColor(Color.WHITE);
-			con.setDrawColor(Color.BLACK);
-			con.drawString("Math Training Game", 380, 215);
-			con.drawString("Play [ P ] ", 380, 250);
-			con.drawString("Leaderboard [ L ]", 380, 285);
-			con.drawString("Add Quiz [ A ]", 380, 320);
-			con.drawString("Quit [ Q ]", 380, 355);
-			con.drawImage(Logo, 385,1);
-			con.repaint();
-		}
+
 	
 //Data
 //ONE MASTER FILE
@@ -98,6 +112,9 @@ public class CPTMarcus{
 		
 		}
 	}
-}
+
+
+
+
 		
 
